c5d61e3e0f97674da5fac5476662a92b86b16a47
Nitad <azash85@gmail.com>
2013-10-08 00:23:50 +0200

    ...

--- TicTacToe/AI_Random_V2.cs
+++ TicTacToe/AI_Random_V2.cs
@@ -28,37 +28,37 @@ public class AI_Random_V2 : AI_Random
         //    Console.WriteLine(tempList[ii]);
         //}
 
-        if (tempList[1] == 'x' && tempList[2] == 'x') { coords = PosToCoords(3); boardPosition = 3; }
-        if (tempList[1] == 'x' && tempList[3] == 'x') { coords = PosToCoords(2); boardPosition = 2; }
-        if (tempList[2] == 'x' && tempList[3] == 'x') { coords = PosToCoords(1); boardPosition = 1; }
+        if (tempList[1] == 'x' && tempList[2] == 'x' && tempList[3] == ' ') { coords = PosToCoords(3); boardPosition = 3; }
+        if (tempList[1] == 'x' && tempList[3] == 'x' && tempList[2] == ' ') { coords = PosToCoords(2); boardPosition = 2; }
+        if (tempList[2] == 'x' && tempList[3] == 'x' && tempList[1] == ' ') { coords = PosToCoords(1); boardPosition = 1; }
 
-        if (tempList[4] == 'x' && tempList[5] == 'x') { coords = PosToCoords(6); boardPosition = 6; }
-        if (tempList[4] == 'x' && tempList[6] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
-        if (tempList[5] == 'x' && tempList[6] == 'x') { coords = PosToCoords(4); boardPosition = 4; }
+        if (tempList[4] == 'x' && tempList[5] == 'x' && tempList[6] == ' ') { coords = PosToCoords(6); boardPosition = 6; }
+        if (tempList[4] == 'x' && tempList[6] == 'x' && tempList[5] == ' ') { coords = PosToCoords(5); boardPosition = 5; }
+        if (tempList[5] == 'x' && tempList[6] == 'x' && tempList[4] == ' ') { coords = PosToCoords(4); boardPosition = 4; }
 
-        if (tempList[7] == 'x' && tempList[8] == 'x') { coords = PosToCoords(9); boardPosition = 9; }
-        if (tempList[7] == 'x' && tempList[9] == 'x') { coords = PosToCoords(8); boardPosition = 8; }
-        if (tempList[8] == 'x' && tempList[9] == 'x') { coords = PosToCoords(7); boardPosition = 7; }
+        if (tempList[7] == 'x' && tempList[8] == 'x' && tempList[9] == ' ') { coords = PosToCoords(9); boardPosition = 9; }
+        if (tempList[7] == 'x' && tempList[9] == 'x' && tempList[8] == ' ') { coords = PosToCoords(8); boardPosition = 8; }
+        if (tempList[8] == 'x' && tempList[9] == 'x' && tempList[7] == ' ') { coords = PosToCoords(7); boardPosition = 7; }
 
-        if (tempList[1] == 'x' && tempList[4] == 'x') { coords = PosToCoords(7); boardPosition = 7; }
-        if (tempList[1] == 'x' && tempList[7] == 'x') { coords = PosToCoords(4); boardPosition = 4; }
-        if (tempList[4] == 'x' && tempList[7] == 'x') { coords = PosToCoords(1); boardPosition = 1; }
+        if (tempList[1] == 'x' && tempList[4] == 'x' && tempList[7] == ' ') { coords = PosToCoords(7); boardPosition = 7; }
+        if (tempList[1] == 'x' && tempList[7] == 'x' && tempList[4] == ' ') { coords = PosToCoords(4); boardPosition = 4; }
+        if (tempList[4] == 'x' && tempList[7] == 'x' && tempList[1] == ' ') { coords = PosToCoords(1); boardPosition = 1; }
 
-        if (tempList[2] == 'x' && tempList[5] == 'x') { coords = PosToCoords(8); boardPosition = 8; }
-        if (tempList[2] == 'x' && tempList[8] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
-        if (tempList[5] == 'x' && tempList[8] == 'x') { coords = PosToCoords(2); boardPosition = 2; }
+        if (tempList[2] == 'x' && tempList[5] == 'x' && tempList[8] == ' ') { coords = PosToCoords(8); boardPosition = 8; }
+        if (tempList[2] == 'x' && tempList[8] == 'x' && tempList[5] == ' ') { coords = PosToCoords(5); boardPosition = 5; }
+        if (tempList[5] == 'x' && tempList[8] == 'x' && tempList[2] == ' ') { coords = PosToCoords(2); boardPosition = 2; }
 
-        if (tempList[3] == 'x' && tempList[6] == 'x') { coords = PosToCoords(9); boardPosition = 9; }
-        if (tempList[3] == 'x' && tempList[9] == 'x') { coords = PosToCoords(6); boardPosition = 6; }
-        if (tempList[6] == 'x' && tempList[9] == 'x') { coords = PosToCoords(3); boardPosition = 3; }
+        if (tempList[3] == 'x' && tempList[6] == 'x' && tempList[9] == ' ') { coords = PosToCoords(9); boardPosition = 9; }
+        if (tempList[3] == 'x' && tempList[9] == 'x' && tempList[6] == ' ') { coords = PosToCoords(6); boardPosition = 6; }
+        if (tempList[6] == 'x' && tempList[9] == 'x' && tempList[3] == ' ') { coords = PosToCoords(3); boardPosition = 3; }
 
-        if (tempList[1] == 'x' && tempList[5] == 'x') { coords = PosToCoords(9); boardPosition = 9; }
-        if (tempList[1] == 'x' && tempList[9] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
-        if (tempList[5] == 'x' && tempList[9] == 'x') { coords = PosToCoords(1); boardPosition = 1; }
+        if (tempList[1] == 'x' && tempList[5] == 'x' && tempList[9] == ' ') { coords = PosToCoords(9); boardPosition = 9; }
+        if (tempList[1] == 'x' && tempList[9] == 'x' && tempList[5] == ' ') { coords = PosToCoords(5); boardPosition = 5; }
+        if (tempList[5] == 'x' && tempList[9] == 'x' && tempList[1] == ' ') { coords = PosToCoords(1); boardPosition = 1; }
 
-        if (tempList[3] == 'x' && tempList[5] == 'x') { coords = PosToCoords(7); boardPosition = 7; }
-        if (tempList[3] == 'x' && tempList[7] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
-        if (tempList[7] == 'x' && tempList[5] == 'x') { coords = PosToCoords(3); boardPosition = 3; }
+        if (tempList[3] == 'x' && tempList[5] == 'x' && tempList[7] == ' ') { coords = PosToCoords(7); boardPosition = 7; }
+        if (tempList[3] == 'x' && tempList[7] == 'x' && tempList[5] == ' ') { coords = PosToCoords(5); boardPosition = 5; }
+        if (tempList[7] == 'x' && tempList[5] == 'x' && tempList[3] == ' ') { coords = PosToCoords(3); boardPosition = 3; }
 
         if (coords[0] == 0)
         {
