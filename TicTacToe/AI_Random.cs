acf2d8cb66c0ae9cb7a7b7b50aef645cf40f8612
Nitad <azash85@gmail.com>
2013-10-07 18:43:54 +0200

    Lade till en Draw() metod som spar lite kod om man ska göra en underklass till AI_Random. Även lagt till Thread.Sleep så datorns drag inte är så snabba

--- TicTacToe/AI_Random.cs
+++ TicTacToe/AI_Random.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 
 public class AI_Random : Player
@@ -10,12 +10,13 @@ public class AI_Random : Player
     public int DrawMarker(SortedList<int, char> list)
     {
         int boardPosition = 0;
-        int[] coords = new int[2];
+        var coords = new int[2];
         int x = 0;
         int y = 0;
         var rnd = new Random();
         int rndNumber = rnd.Next(1, 10);
 
+        //Loop until it finds an empty space on the board
         while (list[rndNumber] != ' ')
         {
             rndNumber = rnd.Next(1, 10);
@@ -26,16 +27,24 @@ public class AI_Random : Player
         y = coords[1];
         boardPosition = rndNumber;
 
-        // Drawing the Cursor depending on playerturn
-        // and chosen char
+        Thread.Sleep(1000);
+
+        Draw(x, y);
+
+        //Return bordPosition for the Board-class to mark that space as occupied.
+        return boardPosition;
+    }
+    public void Draw(int x, int y)
+    {
         Console.SetCursorPosition(x - 1, y - 1);
         Console.Write("{0}{0}{0}", Marker);
         Console.SetCursorPosition(x - 1, y);
         Console.Write("{0} {0}", Marker);
         Console.SetCursorPosition(x - 1, y + 1);
         Console.Write("{0}{0}{0}", Marker);
-        return boardPosition;
+
     }
+    //Translate position (1-9) to coordinates on the board.
     public int[] PosToCoords(int pos)
     {
         var coords = new int[2];
