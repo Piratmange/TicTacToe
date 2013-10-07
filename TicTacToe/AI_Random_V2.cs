d5e19d2f4fc40607d710d23acd2f1975318a6269
Nitad <azash85@gmail.com>
2013-10-07 23:56:40 +0200

    Skit-Tree

--- TicTacToe/AI_Random_V2.cs
+++ TicTacToe/AI_Random_V2.cs
@@ -20,40 +20,44 @@ public class AI_Random_V2 : AI_Random
         //Replaces the opponents marker with 'x'.
         for (int ii = 1; ii < 10; ++ii)
         {
-            if (tempList[ii] != ' ' || tempList[ii] != this.Marker) { tempList[ii] = 'x'; }
+            if (tempList[ii] != ' ' || tempList[ii] == this.Marker) { tempList[ii] = 'x'; }
+        }
+        for (int ii = 1; ii < 10; ++ii)
+        {
+            Console.WriteLine(tempList[ii]);
         }
 
-        //if (tempList[1] == 'x' && tempList[2] == 'x') { coords = PosToCoords(3); boardPosition = 3; }
-        //if (tempList[1] == 'x' && tempList[3] == 'x') { coords = PosToCoords(2); boardPosition = 2; }
-        //if (tempList[2] == 'x' && tempList[3] == 'x') { coords = PosToCoords(1); boardPosition = 1; }
+        if (tempList[1] == 'x' && tempList[2] == 'x') { coords = PosToCoords(3); boardPosition = 3; }
+        if (tempList[1] == 'x' && tempList[3] == 'x') { coords = PosToCoords(2); boardPosition = 2; }
+        if (tempList[2] == 'x' && tempList[3] == 'x') { coords = PosToCoords(1); boardPosition = 1; }
 
-        //if (tempList[4] == 'x' && tempList[5] == 'x') { coords = PosToCoords(6); boardPosition = 6; }
-        //if (tempList[4] == 'x' && tempList[6] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
-        //if (tempList[5] == 'x' && tempList[6] == 'x') { coords = PosToCoords(4); boardPosition = 4; }
+        if (tempList[4] == 'x' && tempList[5] == 'x') { coords = PosToCoords(6); boardPosition = 6; }
+        if (tempList[4] == 'x' && tempList[6] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
+        if (tempList[5] == 'x' && tempList[6] == 'x') { coords = PosToCoords(4); boardPosition = 4; }
 
-        //if (tempList[7] == 'x' && tempList[8] == 'x') { coords = PosToCoords(9); boardPosition = 9; }
-        //if (tempList[7] == 'x' && tempList[9] == 'x') { coords = PosToCoords(8); boardPosition = 8; }
-        //if (tempList[8] == 'x' && tempList[9] == 'x') { coords = PosToCoords(7); boardPosition = 7; }
+        if (tempList[7] == 'x' && tempList[8] == 'x') { coords = PosToCoords(9); boardPosition = 9; }
+        if (tempList[7] == 'x' && tempList[9] == 'x') { coords = PosToCoords(8); boardPosition = 8; }
+        if (tempList[8] == 'x' && tempList[9] == 'x') { coords = PosToCoords(7); boardPosition = 7; }
 
-        //if (tempList[1] == 'x' && tempList[4] == 'x') { coords = PosToCoords(7); boardPosition = 7; }
-        //if (tempList[1] == 'x' && tempList[7] == 'x') { coords = PosToCoords(4); boardPosition = 4; }
-        //if (tempList[4] == 'x' && tempList[7] == 'x') { coords = PosToCoords(1); boardPosition = 1; }
+        if (tempList[1] == 'x' && tempList[4] == 'x') { coords = PosToCoords(7); boardPosition = 7; }
+        if (tempList[1] == 'x' && tempList[7] == 'x') { coords = PosToCoords(4); boardPosition = 4; }
+        if (tempList[4] == 'x' && tempList[7] == 'x') { coords = PosToCoords(1); boardPosition = 1; }
 
-        //if (tempList[2] == 'x' && tempList[5] == 'x') { coords = PosToCoords(8); boardPosition = 8; }
-        //if (tempList[2] == 'x' && tempList[8] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
-        //if (tempList[5] == 'x' && tempList[8] == 'x') { coords = PosToCoords(2); boardPosition = 2; }
+        if (tempList[2] == 'x' && tempList[5] == 'x') { coords = PosToCoords(8); boardPosition = 8; }
+        if (tempList[2] == 'x' && tempList[8] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
+        if (tempList[5] == 'x' && tempList[8] == 'x') { coords = PosToCoords(2); boardPosition = 2; }
 
-        //if (tempList[3] == 'x' && tempList[6] == 'x') { coords = PosToCoords(9); boardPosition = 9; }
-        //if (tempList[3] == 'x' && tempList[9] == 'x') { coords = PosToCoords(6); boardPosition = 6; }
-        //if (tempList[6] == 'x' && tempList[9] == 'x') { coords = PosToCoords(3); boardPosition = 3; }
+        if (tempList[3] == 'x' && tempList[6] == 'x') { coords = PosToCoords(9); boardPosition = 9; }
+        if (tempList[3] == 'x' && tempList[9] == 'x') { coords = PosToCoords(6); boardPosition = 6; }
+        if (tempList[6] == 'x' && tempList[9] == 'x') { coords = PosToCoords(3); boardPosition = 3; }
 
-        //if (tempList[1] == 'x' && tempList[5] == 'x') { coords = PosToCoords(9); boardPosition = 9; }
-        //if (tempList[1] == 'x' && tempList[9] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
-        //if (tempList[5] == 'x' && tempList[9] == 'x') { coords = PosToCoords(1); boardPosition = 1; }
+        if (tempList[1] == 'x' && tempList[5] == 'x') { coords = PosToCoords(9); boardPosition = 9; }
+        if (tempList[1] == 'x' && tempList[9] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
+        if (tempList[5] == 'x' && tempList[9] == 'x') { coords = PosToCoords(1); boardPosition = 1; }
 
-        //if (tempList[3] == 'x' && tempList[5] == 'x') { coords = PosToCoords(7); boardPosition = 7; }
-        //if (tempList[3] == 'x' && tempList[7] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
-        //if (tempList[7] == 'x' && tempList[5] == 'x') { coords = PosToCoords(3); boardPosition = 3; }
+        if (tempList[3] == 'x' && tempList[5] == 'x') { coords = PosToCoords(7); boardPosition = 7; }
+        if (tempList[3] == 'x' && tempList[7] == 'x') { coords = PosToCoords(5); boardPosition = 5; }
+        if (tempList[7] == 'x' && tempList[5] == 'x') { coords = PosToCoords(3); boardPosition = 3; }
 
         if (coords[0] == 0)
         {
@@ -68,9 +72,6 @@ public class AI_Random_V2 : AI_Random
         }
         x = coords[0];
         y = coords[1];
-
-        //MÅSTE LÄGGA TILL BOARDPOSITION IFALL DEN INTE KÖR WHILE-LOOPEN
-
 
         Thread.Sleep(1000);
 
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,22 +6,21 @@ using System.Threading;
 public class program
 {
     static int playerNumber;
-    static Player P1 = new Player("Default", 'C');
-    static Player P2 = new Player("Player 2", 'O');
-    static AI_Random AI1 = new AI_Random("Crappy AI1", 'Z');
-    static AI_Random AI2 = new AI_Random("Crappy AI2", 'V');
-    static AI_Random_V2 AI3 = new AI_Random_V2("IF_AI", 'T');
+    static char buttonPressed;
+    static Player P1;
+    static Player P2;
     static bool playAgain = false;
     static bool firstTime = true;
 
     public static void Main()
     {
+
         var MyBoard = new Board();
         bool playerTurn = true;
 
         if (playAgain == false || firstTime != false)
         {
-            Intro();
+            firstIntro();
         }
 
         Console.Clear();
@@ -30,43 +29,43 @@ public class program
         WinningCount(P1.Name, P1.Winning, 25, 10);
         WinningCount(P2.Name, P2.Winning, 25, 11);
 
-            while (true)
-            {
-                var TempPlayer = playerTurn ? AI3 : P2;
-                playerTurn = !playerTurn;
-                var TempPlayerColor = playerTurn ? Console.ForegroundColor = ConsoleColor.DarkGreen : Console.ForegroundColor = ConsoleColor.DarkRed;
-                MyBoard.MoveOnBoard(TempPlayer);
+        while (true)
+        {
+            var TempPlayer = playerTurn ? P1 : P2;
+            playerTurn = !playerTurn;
+            var TempPlayerColor = playerTurn ? Console.ForegroundColor = ConsoleColor.DarkGreen : Console.ForegroundColor = ConsoleColor.DarkRed;
+            MyBoard.MoveOnBoard(TempPlayer);
 
-                Console.SetCursorPosition(0, 15);
-                if (MyBoard.EvaluateWin(TempPlayer))
+            Console.SetCursorPosition(0, 15);
+            if (MyBoard.EvaluateWin(TempPlayer))
+            {
+                Console.WriteLine("{0} WON!!!                                ", TempPlayer.Name);
+                TempPlayer.Winning++;
+                "Play again Y/N?".Echo();
+                char PlayAnswer = Console.ReadKey().KeyChar;
+                if (PlayAnswer == 'y')
                 {
-                    Console.WriteLine("{0} WON!!!                                ", TempPlayer.Name);
-                    TempPlayer.Winning++;
-                    "Play again Y/N?".Echo();
-                    string PlayAnswer = Console.ReadLine().ToLower();
-                    if (PlayAnswer == "y")
-                    {
-                        playAgain = true;
-                        firstTime = false;
-                        Main();
-                    }
-                    else Environment.Exit(1);
+                    playAgain = true;
+                    firstTime = false;
+                    Main();
                 }
-                else if (!MyBoard.BoardTiles.ContainsValue(' '))
+                else Environment.Exit(1);
+            }
+            else if (!MyBoard.BoardTiles.ContainsValue(' '))
+            {
+                Console.ResetColor();
+                Console.WriteLine("It's a tie                                ");
+                "Play again Y/N?".Echo();
+                char PlayAnswer = Console.ReadKey().KeyChar;
+                if (PlayAnswer == 'y')
                 {
-                    Console.ResetColor();
-                    Console.WriteLine("It's a tie                                ");
-                    "Play again Y/N?".Echo();
-                    string PlayAnswer = Console.ReadLine().ToLower();
-                    if (PlayAnswer == "y")
-                    {
-                        Main();
-                        playAgain = false;
-                        firstTime = false;
-                    }
-                    else break;
+                    playAgain = true;
+                    firstTime = false;
+                    Main();
                 }
-            
+                else break;
+            }
+
         }
     }
 
@@ -74,11 +73,8 @@ public class program
     {
         var playerList = new List<Player>();
 
-        playerList.Add(AI1);
-        playerList.Add(AI2);
         playerList.Add(P1);
         playerList.Add(P2);
-        playerList.Add(AI3);
 
         int number = 1;
         "Choose 2 players".Echo();
@@ -87,23 +83,10 @@ public class program
             Console.Write("{0}. {1} that uses the marker: {2}", number, player.Name, player.Marker + "\n");
             number++;
         }
-        playerNumber = int.Parse(Console.ReadLine());
-        if (playerNumber == 3)
-        {
-            P1 = new Player(Player.GetPlayer(), Player.GetChar());
-            playerList.RemoveAt(2);
-            playerList.Insert(2, P1);
-        }
-        if (playerNumber == 4)
-        {
-            P1 = new Player(Player.GetPlayer(), Player.GetChar());
-            playerList.RemoveAt(3);
-            playerList.Insert(3, P1);
-        }
-        
+
         var chosenPlayer = playerList[playerNumber - 1];
         playerList.RemoveAt(playerNumber - 1);
-        return chosenPlayer;               
+        return chosenPlayer;
     }
 
     public static void WinningCount(string Name, int Wins, int x, int y)
@@ -112,4 +95,34 @@ public class program
         Console.WriteLine(Name + " Wins: " + Wins + "                           ");
     }
 
+    public static void firstIntro()
+    {
+        Console.WriteLine("1. Mänsklig Spelare\n2. Dum Dator\n3. Smart Dator");
+        Console.WriteLine("Välj Spelare 1!");
+        buttonPressed = Console.ReadKey().KeyChar;
+
+        if (buttonPressed == '1')
+        {
+            Console.WriteLine("");
+            P1 = new Player(Player.GetPlayer(), Player.GetChar());
+        }
+        else if (buttonPressed == '2')
+            P1 = new AI_Random("Anna", 'X');
+        else
+            P1 = new AI_Random_V2("Anna", 'X');
+
+        Console.WriteLine("\n1. Mänsklig Spelare\n2. Dum Dator\n3. Smart Dator");
+        Console.WriteLine("Välj Spelare 2!");
+        buttonPressed = Console.ReadKey().KeyChar;
+
+        if (buttonPressed == '1')
+        {
+            Console.WriteLine("");
+            P2 = new Player(Player.GetPlayer(), Player.GetChar());
+        }
+        else if (buttonPressed == '2')
+            P2 = new AI_Random("AnnaBåt", 'O');
+        else
+            P2 = new AI_Random_V2("Anna", 'O');
+    }
 }
